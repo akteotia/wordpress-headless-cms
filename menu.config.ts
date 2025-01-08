96be26ff46bd49ecdce9c66a76208a7ac509ad88
Arun Kumar <arun7245@gmail.com>
2025-01-09 04:00:46 +0530

    Update menu.config.ts

--- menu.config.ts
+++ menu.config.ts
@@ -1,7 +1,7 @@
 // Define the menu items
 export const mainMenu = {
   home: "/",
-  about: "https://github.com/9d8dev/next-wp",
+  about: "https://github.com/akteotia/wordpress-headless-cms",
   blog: "/posts",
 };
 
